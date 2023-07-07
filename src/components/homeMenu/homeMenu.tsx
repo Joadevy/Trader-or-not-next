61e1d2d06c4ff6edf521101bbf43cc9dcec18b5f
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-07-06 21:50:37 -0300

    chore: add initial configs & some styles

---
+++ src/components/homeMenu/homeMenu.tsx
@@ -0,0 +1,12 @@
+const HomeMenu = () => {
+  return (
+    <div className="text-dark-text">
+      <h1 className="text-4xl font-bold text-dark-title">
+        You are a pro trader! Aren&apos;t you?
+      </h1>
+      <p>» Try to find out the price of the cryptocurrency within 5 seconds</p>
+    </div>
+  );
+};
+
+export default HomeMenu;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "dark-bg": "#1a202c",
+        "dark-blue": "#0B2447",
+        "dark-blue-100": "#19376D",
+        "dark-blue-200": "#576CBC",
+        "dark-blue-300": "#A5D7E8",
+        "dark-title": "#FDFDFD",
+        "dark-text": "#E2E8F0",
       },
     },
   },
   plugins: [],
-}
+};
