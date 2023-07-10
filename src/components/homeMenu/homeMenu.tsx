bd72ca39d4f52d2019bd97cb93eaf200c13ce81f
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-07-10 18:59:52 -0300

    feat: add some new options

--- src/components/homeMenu/homeMenu.tsx
+++ src/components/homeMenu/homeMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
 
 import Game from "../Game";
 import HomeButton from "../Buttons/HomeButton";
@@ -8,6 +9,7 @@ import HomeButton from "../Buttons/HomeButton";
 const HomeMenu = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [openFAQ, setOpenFAQ] = useState(false);
+  const [openHistory, setOpenHistory] = useState(false);
 
   const handleStartPlay = () => {
     setIsPlaying(true);
@@ -25,6 +27,12 @@ const HomeMenu = () => {
     setOpenFAQ(false);
   };
 
+  const handleOpenHistory = () => {};
+
+  const handleCloseHistory = () => {
+    setOpenHistory(false);
+  };
+
   if (isPlaying) return <Game backToHome={() => backToHomeFromGame()} />;
 
   // if (openFAQ) return <FAQ />;
@@ -33,6 +41,8 @@ const HomeMenu = () => {
     <div className="text-dark-text flex flex-col gap-2 justify-center">
       <HomeButton handleClick={() => handleStartPlay()} text="Start game" />
       <HomeButton handleClick={() => handleOpenFAQ()} text="FAQ" />
+      {/* <HomeButton handleClick={() => handleOpenHistory()} text="History" /> */}
+      <Link href={"/history"}>History</Link>
       <HomeButton handleClick={() => handleOpenFAQ()} text="My trophies" />
     </div>
   );
