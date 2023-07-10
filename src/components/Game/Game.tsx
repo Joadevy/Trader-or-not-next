b8bad18557d911036fc2a337eb039c2a8ae7551d
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-07-10 17:50:35 -0300

    feat: add result UI

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import HomeButton from "../Buttons/HomeButton";
+import BackButton from "../Buttons/BackButton";
 
 import { CryptoTicker, getRandomCrypto } from "./gameHelpers";
 import Countdown from "./Countdown";
@@ -12,40 +13,55 @@ type Props = {
   backToHome: () => void;
 };
 
+type Result = "win" | "lose" | "draw";
+
 const Game = ({ backToHome }: Props) => {
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [isActiveRound, setIsActiveRound] = useState(false);
   const [score, setScore] = useState(0);
   const [ticker, setTicker] = useState<CryptoTicker>(getRandomCrypto());
-  const [tickerPrice, setTickerPrice] = useState<null | number>(null);
+  const [initialPrice, setInitialPrice] = useState<null | number>(null);
+  const [finalPrice, setFinalPrice] = useState<null | number>(null);
+  const [showResult, setShowResult] = useState(false);
+  const [optionChosen, setOptionChosen] = useState<"higher" | "lower">();
+  const [result, setResult] = useState<Result>();
 
   useEffect(() => {
-    getPrice(ticker.symbol).then((price) => setTickerPrice(price));
+    getPrice(ticker.symbol).then((price) => setInitialPrice(price));
   }, [ticker]);
 
-  const backToHomeFromGame = () => {
-    setIsPlaying(false);
-    backToHome();
-  };
-
-  if (!tickerPrice) return <div className="text-dark-blue-200">Loading...</div>;
+  if (!initialPrice)
+    return <div className="text-dark-blue-200">Loading...</div>;
 
   const higherPriceSelected = async (isHigher: boolean) => {
-    setIsPlaying(true);
+    setIsActiveRound(true);
+    setOptionChosen(isHigher ? "higher" : "lower");
     setTimeout(async () => {
       const newPrice = await getPrice(ticker.symbol);
 
-      if (newPrice > tickerPrice && isHigher) {
-        setScore(score + 1);
-      } else if (newPrice < tickerPrice && !isHigher) {
-        setScore(score + 1);
-      }
+      setFinalPrice(newPrice);
 
-      setIsPlaying(false);
-      // playAnotherRound();
+      if (newPrice > initialPrice && isHigher) {
+        setScore(score + 1);
+        gameResult("win");
+      } else if (newPrice < initialPrice && !isHigher) {
+        setScore(score + 1);
+        gameResult("win");
+      } else if (newPrice === initialPrice) {
+        gameResult("draw");
+      } else {
+        gameResult("lose");
+      }
+      setIsActiveRound(false);
     }, 5000);
   };
 
+  const gameResult = (result: Result) => {
+    setShowResult(true);
+    setResult(result);
+  };
+
   const playAnotherRound = () => {
+    setShowResult(false);
     let newTicker = getRandomCrypto();
 
     while (newTicker.symbol === ticker.symbol) {
@@ -54,33 +70,82 @@ const Game = ({ backToHome }: Props) => {
     setTicker(newTicker);
   };
 
-  const playAgain = () => {
-    setScore(0);
-    setTicker(getRandomCrypto());
-  };
+  // const resetGame = () => {
+  //   setScore(0);
+  //   setShowResult(false);
+  //   setTicker(getRandomCrypto());
+  // };
 
   return (
-    <main className="text-dark-text border border-dark-blue-100 p-4 rounded-lg w-[300px] lg:w-[450px] flex flex-col gap-4 items-center">
-      <header>
-        <div className="font-bold">Score: {score}</div>
-        <h2>
-          Cryptocurrency name:{" "}
-          <span className=" text-dark-blue-300">
-            {ticker.name} ({ticker.shortName})
-          </span>
-        </h2>
-        <h2>
-          Actual Price:{" "}
-          <span className=" text-dark-blue-300">{tickerPrice.toFixed(2)}</span>
-        </h2>
-      </header>
-      {isPlaying ? (
-        <div className="h-10">
-          <p className="text-3xl">
-            <Countdown seconds={5} />
-          </p>
+    <main className="text-dark-text border border-dark-blue-100 p-4 rounded-lg w-[300px] lg:w-[450px] flex flex-col items-center">
+      <div className="flex justify-between w-full">
+        <div>
+          <BackButton
+            description="Back to home"
+            handleClick={() => backToHome()}
+          />
         </div>
-      ) : (
+        <div className="font-bold">Score: {score}</div>
+      </div>
+      <header className="mb-4">
+        {showResult ? (
+          <>
+            <h2>
+              {result === "win"
+                ? "You won!"
+                : result === "lose"
+                ? "You lost!"
+                : "Oops, price didn't change, It's a draw!"}
+            </h2>
+            <h2>
+              Cryptocurrency name:{" "}
+              <span className=" text-dark-blue-300">
+                {ticker.name} ({ticker.shortName})
+              </span>
+            </h2>
+            <h3>
+              Initial Price:{" "}
+              <span className=" text-dark-blue-300">
+                {initialPrice.toFixed(2)}
+              </span>
+            </h3>
+            <h4>
+              Final Price:{" "}
+              <span className=" text-dark-blue-300">
+                {finalPrice!.toFixed(2)}
+              </span>
+            </h4>
+            <p>
+              & you selected{" "}
+              <span className="text-dark-blue-300">{optionChosen} price</span>
+            </p>
+
+            <div className="text-center mt-2">
+              <HomeButton
+                handleClick={() => playAnotherRound()}
+                text="Play again"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <h2>
+              Cryptocurrency name:{" "}
+              <span className=" text-dark-blue-300">
+                {ticker.name} ({ticker.shortName})
+              </span>
+            </h2>
+            <h3>
+              Actual Price:{" "}
+              <span className=" text-dark-blue-300">
+                {initialPrice.toFixed(2)}
+              </span>
+            </h3>
+          </>
+        )}
+      </header>
+      {isActiveRound && <Countdown seconds={5} />}
+      {!isActiveRound && !showResult && (
         <div className="flex gap-2 h-10">
           <HomeButton
             handleClick={() => higherPriceSelected(true)}
