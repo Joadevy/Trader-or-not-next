2f8e416b69f8672ba2885854e10ce86af9c7c319
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-07-11 18:02:22 -0300

    feat: replace home buttons to crypto buttons

--- src/components/Game/ButtonsTradeOptions.tsx
+++ src/components/Game/ButtonsTradeOptions.tsx
@@ -1,18 +1,29 @@
+import type { CryptoTicker } from "@/components/Game/gameHelpers";
+
+import CryptoButton from "../Buttons/CryptoButton";
 import HomeButton from "../Buttons/HomeButton";
 
 type Props = {
   higherPriceSelected: (_: boolean) => void;
+  shortNameTicker: CryptoTicker["shortName"];
 };
 
-const ButtonsTradeOptions = ({ higherPriceSelected }: Props) => {
+const ButtonsTradeOptions = ({
+  higherPriceSelected,
+  shortNameTicker,
+}: Props) => {
   return (
     <div className="flex gap-2 h-10">
-      <HomeButton
+      <CryptoButton
         handleClick={() => higherPriceSelected(true)}
+        isHigher={true}
+        shortNameTicker={shortNameTicker}
         text="Higher price"
       />
-      <HomeButton
+      <CryptoButton
         handleClick={() => higherPriceSelected(false)}
+        isHigher={false}
+        shortNameTicker={shortNameTicker}
         text="Lower price"
       />
     </div>
