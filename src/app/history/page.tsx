168ba94157a8109c7a6a85b4aa1745c01b252169
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-07-12 18:40:42 -0300

    feat: divide streaks per game

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,9 +1,36 @@
 "use client";
 
-import type { TradeResult } from "@/components/Game/gameHelpers";
+import type { StreakTradeResult } from "@/components/Game/gameHelpers";
 
-const splitStreakForGameID = (trades: TradeResult[]) => {
-  const streaks = new Map<TradeResult["gameId"], TradeResult[]>();
+const splitStreakForStreakID = (
+  MapGameIdStreaks: Map<StreakTradeResult["gameId"], StreakTradeResult[]>,
+) => {
+  const streaks = new Map<
+    StreakTradeResult["gameId"],
+    Map<StreakTradeResult["streakId"], StreakTradeResult[]>
+  >();
+
+  Array.from(MapGameIdStreaks.values()).map((trades) => {
+    trades.forEach((trade) => {
+      if (streaks.has(trade.gameId)) {
+        streaks.set(
+          trade.gameId,
+          streaks
+            .get(trade.gameId)!
+            .set(trade.streakId, [
+              ...(streaks.get(trade.gameId)!.get(trade.streakId) ?? []),
+              trade,
+            ]),
+        );
+      } else streaks.set(trade.gameId, new Map([[trade.streakId, [trade]]]));
+    });
+  });
+
+  return streaks;
+};
+
+const splitStreakForGameID = (trades: StreakTradeResult[]) => {
+  const streaks = new Map<StreakTradeResult["gameId"], StreakTradeResult[]>();
 
   trades.forEach((trade) => {
     if (streaks.has(trade.gameId)) {
@@ -11,15 +38,15 @@ const splitStreakForGameID = (trades: TradeResult[]) => {
     } else streaks.set(trade.gameId, [trade]);
   });
 
-  return streaks;
+  return splitStreakForStreakID(streaks);
 };
 
 const page = () => {
   const historyTrades = localStorage.getItem("tradesInARow")
-    ? (JSON.parse(localStorage.getItem("tradesInARow")!) as TradeResult[])
+    ? (JSON.parse(localStorage.getItem("tradesInARow")!) as StreakTradeResult[])
     : [];
 
-  const streaks = splitStreakForGameID(historyTrades);
+  const streaksPerGame = splitStreakForGameID(historyTrades);
 
   return (
     <main className="p-4">
@@ -27,41 +54,48 @@ const page = () => {
         Successful streak of trades
       </h1>
       <div className=" text-dark-text flex gap-6 flex-wrap">
-        {Array.from(splitStreakForGameID(historyTrades).keys()).map(
-          (gameID, index) => (
-            <div
-              key={gameID}
-              className="border border-dark-blue-100 p-2 rounded-md"
-            >
-              <h2>Game {index + 1}</h2>
-              <div className="flex gap-2">
-                {streaks.get(gameID)?.map((trade, index) => (
-                  <div
-                    key={index}
-                    className="border border-dark-blue-200 p-2 rounded-md bg-dark-blue-100"
-                  >
-                    <p>Crypto: {trade.ticker.name}</p>
-                    <p>Initial price: {trade.initialPrice}</p>
-                    <p>Final price: {trade.finalPrice}</p>
-                    <p>
-                      Selection:{" "}
-                      <span
-                        className={
-                          trade.selection === "higher"
-                            ? "text-green-300"
-                            : "text-red-300"
-                        }
-                      >
-                        {trade.selection} price
-                      </span>
-                    </p>
-                    <p>{new Date(trade.date).toLocaleString()}</p>
+        {Array.from(streaksPerGame.keys()).map((gameID, index) => (
+          <div
+            key={gameID}
+            className="border border-dark-blue-100 p-2 rounded-md"
+          >
+            <h2 className="font-bold">Game {index + 1}</h2>
+            <div className="flex gap-4">
+              {Array.from(streaksPerGame.get(gameID)!.values()).map(
+                (strake, index) => (
+                  <div key={index} className="flex flex-col">
+                    <h3 className="text-sm">Streak {index + 1}</h3>
+                    <div className="flex gap-1">
+                      {strake.map((trade, index) => (
+                        <div
+                          key={index}
+                          className="border border-dark-blue-200 p-2 rounded-md bg-dark-blue-100"
+                        >
+                          <p>Crypto: {trade.ticker.name}</p>
+                          <p>Initial price: {trade.initialPrice}</p>
+                          <p>Final price: {trade.finalPrice}</p>
+                          <p>
+                            Selection:{" "}
+                            <span
+                              className={
+                                trade.selection === "higher"
+                                  ? "text-green-300"
+                                  : "text-red-300"
+                              }
+                            >
+                              {trade.selection} price
+                            </span>
+                          </p>
+                          <p>{new Date(trade.date).toLocaleString()}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                ))}
-              </div>
+                ),
+              )}
             </div>
-          ),
-        )}
+          </div>
+        ))}
       </div>
     </main>
   );
