7bf7e23e5d12ba90a5ac2d2962ca062c9c69d281
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-07-21 21:45:09 -0300

    style: add mobile view to history page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -56,7 +56,7 @@ const page = () => {
 
   return (
     <main className="p-4">
-      <h1 className=" text-dark-title text-2xl mb-2">
+      <h1 className=" text-dark-title text-2xl mb-4 lg:mb-2">
         Successful streak of trades
       </h1>
       <div className=" text-dark-text flex gap-6 flex-wrap items-center ">
@@ -66,16 +66,16 @@ const page = () => {
             className="p-2 border border-dark-blue rounded-md shadow-[0_0_15px_#0B2447]"
           >
             <h2 className="font-bold">Game {index + 1}</h2>
-            <div className="flex gap-4">
+            <div className="flex flex-col gap-4 lg:flex-row">
               {Array.from(streaksPerGame.get(gameID)!.values()).map(
                 (strake, index) => (
                   <div key={index} className="flex flex-col">
                     <h3 className="text-sm">Streak {index + 1}</h3>
-                    <div className="flex gap-1">
+                    <div className="flex gap-2 flex-wrap lg:gap-1">
                       {strake.map((trade, index) => (
                         <div
                           key={index}
-                          className="border border-dark-blue-100 p-2 rounded-md bg-dark-blue"
+                          className="border border-dark-blue-100 p-2 rounded-md bg-dark-blue w-[150px] lg:w-fit"
                         >
                           <p>Crypto: {trade.ticker.name}</p>
                           <p>Initial price: {trade.initialPrice}</p>
