e04b886f69c2d82465a36f51c22e1bec39824983
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-07-22 17:07:51 -0300

    feat: add scroll to when game starts

--- src/components/homeMenu/homeMenu.tsx
+++ src/components/homeMenu/homeMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Game from "../Game";
 import HomeButton from "../Buttons/HomeButton";
@@ -8,9 +8,34 @@ import LinkButton from "../Buttons/LinkButton";
 
 const HomeMenu = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [scrollToSection, setScrollToSection] = useState(false);
+
+  useEffect(() => {
+    const handleScrollToSection = () => {
+      if (scrollToSection) {
+        const section = document.getElementById("buttonsHigherLower");
+
+        if (section) {
+          const yOffset = -75;
+          const y =
+            section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+          window.scrollTo({ top: y, behavior: "smooth" });
+        }
+        setScrollToSection(false);
+      }
+    };
+
+    const timer = setTimeout(handleScrollToSection, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [scrollToSection]);
 
   const handleStartPlay = () => {
     setIsPlaying(true);
+    setScrollToSection(true);
   };
 
   const backToHomeFromGame = () => {
