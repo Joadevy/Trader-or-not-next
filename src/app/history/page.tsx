39c43d3d4a9f10cb3fe32c4189b45bfba93c20a3
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-07-27 20:42:58 -0300

    chore: extract functions to helpers

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -2,44 +2,8 @@
 
 import type { StreakTradeResult } from "@/components/Game/gameHelpers";
 
-const splitStreakForStreakID = (
-  MapGameIdStreaks: Map<StreakTradeResult["gameId"], StreakTradeResult[]>,
-) => {
-  const streaks = new Map<
-    StreakTradeResult["gameId"],
-    Map<StreakTradeResult["streakId"], StreakTradeResult[]>
-  >();
-
-  Array.from(MapGameIdStreaks.values()).map((trades) => {
-    trades.forEach((trade) => {
-      if (streaks.has(trade.gameId)) {
-        streaks.set(
-          trade.gameId,
-          streaks
-            .get(trade.gameId)!
-            .set(trade.streakId, [
-              ...(streaks.get(trade.gameId)!.get(trade.streakId) ?? []),
-              trade,
-            ]),
-        );
-      } else streaks.set(trade.gameId, new Map([[trade.streakId, [trade]]]));
-    });
-  });
-
-  return streaks;
-};
-
-const splitStreakForGameID = (trades: StreakTradeResult[]) => {
-  const streaks = new Map<StreakTradeResult["gameId"], StreakTradeResult[]>();
-
-  trades.forEach((trade) => {
-    if (streaks.has(trade.gameId)) {
-      streaks.set(trade.gameId, [...streaks.get(trade.gameId)!, trade]);
-    } else streaks.set(trade.gameId, [trade]);
-  });
-
-  return splitStreakForStreakID(streaks);
-};
+// eslint-disable-next-line import/no-unresolved
+import { splitStreakForGameID } from "@/utils/helpers";
 
 const page = () => {
   let historyTrades: StreakTradeResult[] = [];
